252ea00f5da79e6ace7230b52b52847c7d2aa47c
EmiroMunoz01 <emiro_2001@icloud.com>
2025-01-28 11:01:59 -0500

    Se ha creado el metedo para encontrar un usuario por el nombre de usuario, se ha creado directamente en el repositorio

--- src/main/java/com/security/spring_jwt/Repositories/UserRepository.java
+++ src/main/java/com/security/spring_jwt/Repositories/UserRepository.java
@@ -4,7 +4,11 @@ import com.security.spring_jwt.Entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
+
+    Optional<UserEntity> findByUsername(String username);
 }
