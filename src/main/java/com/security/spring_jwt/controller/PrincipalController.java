bd44288d4cde02d2218346e0943c0bd819ae5168
EmiroMunoz01 <emiro_2001@icloud.com>
2025-01-28 20:13:10 -0500

    De momento estamos autenticando con las credenciales que asignamos en el userDetails, pero no tenemos encriptacion, cuidado.

---
+++ src/main/java/com/security/spring_jwt/controller/PrincipalController.java
@@ -0,0 +1,69 @@
+package com.security.spring_jwt.controller;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.security.spring_jwt.DTO.CreateUserDTO;
+import com.security.spring_jwt.Entity.ERole;
+import com.security.spring_jwt.Entity.RoleEntity;
+import com.security.spring_jwt.Entity.UserEntity;
+import com.security.spring_jwt.Repositories.UserRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+public class PrincipalController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World Not Secured";
+    }
+
+    @GetMapping("/hello-secured")
+    public String helloSecured() {
+        return "Hello World Secured";
+    }
+
+    @PostMapping("/create-user")
+    public ResponseEntity<?> createUser(@Valid @RequestBody CreateUserDTO createUserDTO) {
+
+        Set<RoleEntity> roles = createUserDTO.getRoles().stream()
+                .map(role -> RoleEntity.builder()
+                        .name(ERole.valueOf(role))
+                        .build())
+                .collect(Collectors.toSet());
+
+        UserEntity userEntity = UserEntity.builder()
+                .username(createUserDTO.getUsername())
+                .password(createUserDTO.getPassword())
+                .email(createUserDTO.getEmail())
+                .roles(roles)
+                .build();
+
+        userRepository.save(userEntity);
+
+        return ResponseEntity.ok(userEntity);
+
+    }
+
+    @DeleteMapping("/delete-user")
+  
+    public String deleteUser(@RequestParam String id) {
+        userRepository.deleteById(Long.parseLong(id));
+        return "Se ha eliminado el usuario con id " + id;
+    }
+
+}
---
+++ src/main/java/com/security/spring_jwt/security/SecurityConfig.java
@@ -0,0 +1,74 @@
+package com.security.spring_jwt.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.cglib.proxy.NoOp;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // aqui estamos configurando el comportamiento de acceso a los endpoints, el
+    // manejo de la sesion y tenemos una autenticacion basica, esta autenticacion
+    // basica se hara con un usuario en memoria que hemos creado(1), pero para que
+    // ese usuario pueda funcionar debe ser administrado por algun objeto que
+    // administre la autenticacion, para eso tenemos el autenticationManager(2)
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/hello").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(withDefaults())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return httpSecurity.build();
+
+    }
+
+    // 1 se define el usuario en memoria que usaremos de forma temporal
+    @Bean
+    UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("emiro")
+                .password("12345")
+                .roles()
+                .build());
+
+        return manager;
+    }
+
+    // 3
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        //esto lo colocamos como formalidad, porque en realidad aun no estamos encriptando nada
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    // 2, este autentication usa como parametros el passwordEncoder que hemos
+    // declarado(3), nos exige manejar un passwordEnconder
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder)
+            throws Exception {
+
+        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder)
+                .and().build();
+    }
+
+}
